85680aa8285fc28ad1cbc0524c84b573513fc6fd
Sam Phillips <samdphillips@gmail.com>
2014-01-10 14:26:38 -0800

    Ported Stream class from ding

---
+++ onyx/util/stream.py
@@ -0,0 +1,115 @@
+
+
+class EmptyStreamError(Exception):
+    pass
+
+
+class EmptyState(object):
+    def is_empty(self, stream):
+        return True
+
+    def value(self, stream):
+        raise EmptyStreamError()
+
+    def rest(self, stream):
+        raise EmptyStreamError()
+
+
+EMPTY = EmptyState()
+
+
+class PendingState(object):
+    __slots__ = ['_iterator']
+
+    def __init__(self, iterator):
+        self._iterator = iterator
+
+    def value(self, stream):
+        state = self.force(stream)
+        return state.value(stream)
+
+    def rest(self, stream):
+        state = self.force(stream)
+        return state.rest(stream)
+
+    def is_empty(self, stream):
+        state = self.force(stream)
+        return state.is_empty(stream)
+
+    def force(self, stream):
+        try:
+            v = self._iterator.__next__()
+            s = ValueState(v, self)
+        except StopIteration:
+            s = EMPTY
+        stream._state = s
+        return s
+
+
+class ValueState(object):
+    __slots__ = ['_value', '_rest']
+
+    def __init__(self, value, rest):
+        self._value = value
+        self._rest = Stream(rest)
+
+    def value(self, stream):
+        return self._value
+
+    def rest(self, stream):
+        return self._rest
+
+    def is_empty(self, stream):
+        return False
+
+
+class Stream(object):
+    __slots__ = ['_state']
+
+    @classmethod
+    def from_sequence(cls, it):
+        return cls(PendingState(iter(it)))
+
+    @classmethod
+    def from_file(cls, filename):
+        f = file(filename, 'r')
+        it = CharIoIterator(f)
+        return cls.from_iterator(it)
+
+    def __init__(self, state):
+        self._state = state
+
+    @property
+    def first(self):
+        return self._state.value(self)
+
+    @property
+    def rest(self):
+        return self._state.rest(self)
+
+    @property
+    def is_empty(self):
+        return self._state.is_empty(self)
+
+    def __iter__(self):
+        s = self
+        while not s.is_empty:
+            yield s.first
+            s = s.rest
+        raise StopIteration
+
+
+class CharIoIterator(object):
+    def __init__(self, io):
+        self._io = io
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        c = self._io.read(1)
+        if c == '':
+            raise StopIteration
+        return c
+
+
---
+++ tests/util/test_stream.py
@@ -0,0 +1,76 @@
+
+
+import unittest
+
+
+class IteratorStreamTests(unittest.TestCase):
+    def setUp(self):
+        from onyx.util.stream import Stream
+        self.stream = Stream.from_sequence(range(10))
+
+    def test_first(self):
+        self.assertEqual(self.stream.first, 0)
+        self.assertEqual(self.stream.first, 0)
+
+    def test_rest_first(self):
+        self.assertEqual(self.stream.rest.first, 1)
+        self.assertEqual(self.stream.rest.first, 1)
+
+    def test_rest_rest_first(self):
+        self.assertEqual(self.stream.rest.rest.first, 2)
+        self.assertEqual(self.stream.rest.rest.first, 2)
+
+    def test_is_empty(self):
+        self.assertFalse(self.stream.is_empty)
+
+    def test_all_read_is_empty(self):
+        s = self.stream
+        for n in range(10):
+            self.assertEqual(s.first, n)
+            s = s.rest
+        self.assertTrue(s.is_empty)
+
+
+class EmptyStreamTests(unittest.TestCase):
+    def setUp(self):
+        from onyx.util.stream import Stream
+        self.stream = Stream.from_sequence([])
+
+    def test_is_empty(self):
+        self.assertTrue(self.stream.is_empty)
+
+    def test_first(self):
+        from onyx.util.stream import EmptyStreamError
+        with self.assertRaises(EmptyStreamError):
+            self.stream.first
+
+    def test_rest(self):
+        from onyx.util.stream import EmptyStreamError
+        with self.assertRaises(EmptyStreamError):
+            self.stream.rest
+
+
+class IteratorTests(unittest.TestCase):
+    def setUp(self):
+        from onyx.util.stream import Stream
+        self.stream = Stream.from_sequence(range(10))
+
+    def test_iterate_over_values(self):
+        for x,y in zip(self.stream, range(10)):
+            self.assertEqual(x, y)
+        self.assertEqual(self.stream.first, 0)
+
+
+class CharIoIteratorTests(unittest.TestCase):
+    def setUp(self):
+        from io import StringIO
+        from onyx.util.stream import CharIoIterator
+        sio = StringIO('0123456789')
+        self.cio_iter = CharIoIterator(sio)
+
+    def test_iteration(self):
+        s = '0123456789'
+        for c1, c2 in zip(self.cio_iter, s):
+            self.assertEqual(c1, c2)
+        
+
