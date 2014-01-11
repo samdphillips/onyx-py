7da767ea159693819bd416ef51b4d21d427286e6
Sam Phillips <samdphillips@gmail.com>
2014-01-11 11:13:12 -0800

    Start of a basic Reader
    - added identifiers and keywords to Reader
    - added character classification helper class
    - added some basic Term types

---
+++ onyx/reader.py
@@ -0,0 +1,97 @@
+
+import operator
+
+class Term(object):
+    is_id = False
+    is_keyword = False
+
+
+class NamedTerm(Term):
+    def __init__(self, name):
+        self.name = name
+
+
+class IdTerm(NamedTerm):
+    is_id = True
+
+
+class KeywordTerm(NamedTerm):
+    is_keyword = True
+
+
+class Classifier(object):
+    def __init__(self):
+        self._classifiers = []
+
+    def add_func(self, func, names):
+        self._classifiers.append((func, set(names)))
+
+    def add_test(self, testname, names):
+        self.add_func(operator.methodcaller(testname), names)
+
+    def add_chars(self, chars, names):
+        self.add_func(chars.__contains__, names)
+
+    def classify(self, char):
+        cls = set()
+        for cl_func, cl_names in self._classifiers:
+            if cl_func(char):
+                cls.update(cl_names)
+        return cls
+
+
+_default_classifier = Classifier()
+_default_classifier.add_test('isspace', ['space'])
+_default_classifier.add_test('isalpha', ['idchar'])
+_default_classifier.add_test('isdigit', ['idchar', 'digit'])
+_default_classifier.add_chars('_!?', ['idchar'])
+
+
+class Reader(object):
+    _classifier = _default_classifier
+
+    def __init__(self, stream):
+        self._stream = stream
+
+    def is_at_end(self):
+        return self._stream.is_empty
+
+    def current_char(self):
+        if self.is_at_end():
+            return ''
+        return self._stream.first
+
+    def current_class(self):
+        if self.is_at_end():
+            return set(['eof'])
+        return self._classifier.classify(self.current_char())
+
+    def step(self):
+        self._stream = self._stream.rest
+
+    def is_space(self):
+        return 'space' in self.current_class()
+
+    def is_idchar(self):
+        return 'idchar' in self.current_class()
+
+    def read_space(self):
+        while self.is_space():
+            self.step()
+
+    def read_id_or_kw(self):
+        s = ''
+        while self.is_idchar():
+            s += self.current_char()
+            self.step()
+
+        if self.current_char() == ':':
+            s += self.current_char()
+            return KeywordTerm(s)
+        return IdTerm(s)
+
+
+
+
+
+
---
+++ tests/test_reader.py
@@ -0,0 +1,74 @@
+
+
+import unittest
+
+
+class TestReader(unittest.TestCase):
+    def init_reader(self, s):
+        from onyx.util.stream import Stream
+        from onyx.reader import Reader
+        stream = Stream.from_sequence(s)
+        self.reader = Reader(stream)
+
+    def assert_at_end(self):
+        if not self.reader.is_at_end():
+            self.fail('reader is not at end')
+
+    def test_classify_eof(self):
+        "reading at eof should produce the proper classification"
+        self.init_reader('')
+        self.assertEqual(set(['eof']), self.reader.current_class())
+        self.assert_at_end()
+
+    def test_read_space_at_end(self):
+        "reading spaces stops at end"
+        self.init_reader('    ')
+        self.reader.read_space()
+        self.assert_at_end()
+
+    def test_read_space_not_at_end(self):
+        "reading space just consumes spaces"
+        self.init_reader('    a')
+        self.reader.read_space()
+        self.assertEqual(self.reader.current_char(), 'a')
+
+    def test_read_id(self):
+        "ids can contain letters"
+        self.init_reader('abcd')
+        t = self.reader.read_id_or_kw()
+        self.assertTrue(t.is_id)
+        self.assertEqual(t.name, 'abcd')
+
+    def test_read_id_underscore(self):
+        "ids can contain underscore"
+        self.init_reader('_abcd')
+        t = self.reader.read_id_or_kw()
+        self.assertTrue(t.is_id)
+        self.assertEqual(t.name, '_abcd')
+
+    def test_read_id_underscore_digit(self):
+        "ids can contain underscore and digits"
+        self.init_reader('a_1')
+        t = self.reader.read_id_or_kw()
+        self.assertTrue(t.is_id)
+        self.assertEqual(t.name, 'a_1')
+
+    def test_read_id_punct_exclaim(self):
+        "ids can contain exclamation points"
+        self.init_reader("mutate!")
+        t = self.reader.read_id_or_kw()
+        self.assertTrue(t.is_id)
+        self.assertEqual(t.name, 'mutate!')
+
+    def test_read_id_punct_question(self):
+        "ids can contain question marks"
+        self.init_reader("really?")
+        t = self.reader.read_id_or_kw()
+        self.assertTrue(t.is_id)
+        self.assertEqual(t.name, 'really?')
+
+    def test_read_keyword(self):
+        self.init_reader("do:")
+        t = self.reader.read_id_or_kw()
+        self.assertTrue(t.is_keyword)
+        self.assertEqual(t.name, 'do:')
