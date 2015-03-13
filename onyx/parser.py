673a77b0acdcd24db4589ce61a0be8fbb045b5ff
Sam Phillips <samdphillips@gmail.com>
2015-03-13 13:49:47 -0700

    Added expectation methods to Parser.
    Added methods to check input stream for properties on the incoming Terms
    to sanity check the input.

--- onyx/parser.py
+++ onyx/parser.py
@@ -15,6 +15,29 @@ class Parser(object):
     def step(self):
         self.stream = self.stream.rest
 
+    def at_end(self):
+        return self.stream.is_empty
+
+    def assert_term_value(self, value):
+        term = self.stream.first
+        term_value = term.value
+        if term_value != value:
+            raise ParseError("Expected term value: {0}, "
+                             "got: {1}".format(value, term_value))
+
+    def assert_term_compound(self, shape):
+        term = self.stream.first
+        if not (term.is_compound and term.shape == shape):
+            raise ParseError("Expected compound with shape {0!r}, "
+                             "got: {1}".format(shape, term))
+
+    def assert_term_kind(self, kind):
+        term = self.stream.first
+        value = getattr(term, kind, False)
+        if not value:
+            raise ParseError(
+                "Expected term kind {0!r}, got: {1}".format(kind, term))
+
     def parse_primary(self):
         try:
             term = self.stream.first
